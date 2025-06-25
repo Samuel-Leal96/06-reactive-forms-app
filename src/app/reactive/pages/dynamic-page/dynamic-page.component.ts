c43566328f2bc507398e74c554ff998c819693db
Samuel-Leal96 <saam_lv96@hotmail.com>
2025-06-25 15:39:40 -0600

    Formularios dinámicos con arreglos

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,12 +1,33 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
   imports: [
-    JsonPipe
+    JsonPipe,
+    ReactiveFormsModule
   ],
   templateUrl: './dynamic-page.component.html',
   styleUrl: './dynamic-page.component.scss',
 })
-export class DynamicPageComponent { }
+export class DynamicPageComponent {
+
+  private fb = inject(FormBuilder)
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array(
+      [
+        ['Metal Gear', [Validators.required, Validators.minLength(3)]],
+        ['Call of Duty', [Validators.required, Validators.minLength(3)]]
+      ],
+      Validators.minLength(3) //* Minimo debe haber 3 juegos en el arreglo que se manda
+    )
+  });
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray
+  }
+
+}

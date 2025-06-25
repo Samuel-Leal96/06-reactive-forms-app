a3f85b4c8cbba65d367319f80a53d9bcf6bde4d3
Samuel-Leal96 <saam_lv96@hotmail.com>
2025-06-25 13:13:43 -0600

    Mostrar errores en pantalla
    Se muestran los errores en pantalla del input name

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -15,7 +15,7 @@ export class BasicPageComponent {
 
   private fb = inject(FormBuilder);
 
-  myForm = this.fb.group({
+  myForm: FormGroup = this.fb.group({
     //* name: ['', /** Validadores sincronos */, /** Validadores asincronos */ ],
     name: ['', [Validators.required, Validators.minLength(5)], ],
     price: [0, [Validators.required, Validators.min(10)]],
@@ -27,5 +27,28 @@ export class BasicPageComponent {
   //   price: new FormControl(0),
   //   inStorage: new FormControl(0),
   // })
+
+  isValidField(fieldName: string): boolean | null{
+    return !!this.myForm.controls[fieldName].errors
+  }
+
+  getFieldError(fieldName: string): string | null{
+    const errors = this.myForm.controls[fieldName].errors ?? {};
+
+    for(const key of Object.keys(errors)){ //* Obtenemos todas las keys del objeto de errores
+      switch(key){
+        case 'required':
+          return 'Este campo es requerido'
+
+        case 'minlength':
+          return `Minimo de ${errors['minlength'].requiredLength} caracteres`;
+
+        case 'min':
+          return `Valor minimo de ${errors['min'].min}`
+      }
+    }
+
+    return null;
+  }
 
 }

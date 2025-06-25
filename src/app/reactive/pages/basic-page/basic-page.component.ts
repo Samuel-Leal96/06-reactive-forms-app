6b13420cb16b1dd8efdaba78b8b63d2dd0218e52
Samuel-Leal96 <saam_lv96@hotmail.com>
2025-06-25 10:38:54 -0600

    Form Builder
    Se agrega el form builder al formulatio

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,31 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
   imports: [
-    JsonPipe
+    JsonPipe,
+    ReactiveFormsModule
   ],
   templateUrl: './basic-page.component.html',
   styleUrl: './basic-page.component.scss',
 })
-export class BasicPageComponent { }
+export class BasicPageComponent {
+
+  private fb = inject(FormBuilder);
+
+  myForm = this.fb.group({
+    //* name: ['', /** Validadores sincronos */, /** Validadores asincronos */ ],
+    name: ['', /** Validadores sincronos */, /** Validadores asincronos */ ],
+    price: [0],
+    inStorage: [0],
+  })
+
+  // myForm = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // })
+
+}

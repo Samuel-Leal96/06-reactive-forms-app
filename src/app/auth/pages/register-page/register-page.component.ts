16fda9eb7c8d92a2d7f3ab8e8dd2e68e3089d4e6
Samuel-Leal96 <saam_lv96@hotmail.com>
2025-06-24 21:20:51 -0600

    Ruta padre y template html
    Se crean las rutas de las paginas, el menú de navegación y se renderizan los templates con el gist que nos dan en el curso

---
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -0,0 +1,12 @@
+import { JsonPipe } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-page',
+  imports: [
+    JsonPipe
+  ],
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.scss',
+})
+export class RegisterPageComponent { }

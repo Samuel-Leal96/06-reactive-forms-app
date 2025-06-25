e5bbace80569efff85099443d0444157df69b004
Samuel-Leal96 <saam_lv96@hotmail.com>
2025-06-25 16:05:18 -0600

    Dynamic Forms: Mostrar errores en pantalla
    Se agregan los errores en pantalla para los inputs y se refactoriza el codigo para mostrar el texto a mostrar por error en una funcion estatica

--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,18 +1,16 @@
-import { FormGroup } from '@angular/forms';
-export class FormUtils{
+import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
+export class FormUtils {
 
-  static isValidField(form: FormGroup, fieldName: string): boolean | null{
+  static isValidField(form: FormGroup, fieldName: string): boolean | null {
     return (
       !!form.controls[fieldName].errors &&
       form.controls[fieldName].touched
     )
   }
 
-  static getFieldError(form: FormGroup , fieldName: string): string | null{
-    const errors = form.controls[fieldName].errors ?? {};
-
-    for(const key of Object.keys(errors)){ //* Obtenemos todas las keys del objeto de errores
-      switch(key){
+  static getTextError(errors: ValidationErrors){
+    for (const key of Object.keys(errors)) { //* Obtenemos todas las keys del objeto de errores
+      switch (key) {
         case 'required':
           return 'Este campo es requerido'
 
@@ -25,6 +23,27 @@ export class FormUtils{
     }
 
     return null;
+  }
+
+  static getFieldError(form: FormGroup, fieldName: string): string | null {
+    const errors = form.controls[fieldName].errors ?? {};
+
+    return FormUtils.getTextError(errors);
+  }
+
+  static isValidFieldInArray(formArray: FormArray, index: number) {
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+    );
+  }
+
+  static getFieldErrorInArray(formArray: FormArray, index: number): string | null {
+
+    if(formArray.length === 0) return null;
+
+    const errors = formArray.controls[index].errors ?? {};
+
+    return FormUtils.getTextError(errors);
   }
 
 
